6b4a0474e6e4c136f0c8cd419e73bf49fdd08631
Adesoye AbdulMumeen <abdulmumeenadesoye19@gmail.com>
2025-09-01 13:08:44 +0100

    Another commit

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,5 @@
-import '../styles/globals.css';
-import { Rubik } from 'next/font/google';
+import '../styles/globals.css'; 
+export default function App({ Component, pageProps }) { 
+  return <Component {...pageProps} />; 
+};
 
-// Load Rubik font (weights: 400, 500, 700)
-const rubik = Rubik({ subsets: ['latin'], weight: ['400', '500', '700'] });
-
-export default function App({ Component, pageProps }) {
-  return (
-    <main className={rubik.className}>
-      <Component {...pageProps} />
-    </main>
-  );
-}
